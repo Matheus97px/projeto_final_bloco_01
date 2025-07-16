3eea88cb3b6fd4ea2d7a5f269e344c221300bce1
Matheus97px <matheus97p.q@gmail.com>
2025-07-16 10:45:43 -0300

    Adiciona menu da loja

---
+++ Menu.ts
@@ -0,0 +1,65 @@
+import readLine = require("readline-sync");
+
+export function main() {
+
+    let opcao: number;
+
+    while (true) {
+
+        console.log(`*****************************************************`);
+        console.log(`                                                     `);
+        console.log(`                    Retro Games                      `);
+        console.log(`                                                     `);
+        console.log(`*****************************************************`);
+        console.log(`                                                     `);
+        console.log(`            1 - Adicionar Novo Jogo                  `);
+        console.log(`            2 - Exibir Coleção de Jogos              `);
+        console.log(`            3 - Procurar um Jogo                     `);
+        console.log(`            4 - Editar um Jogo                       `);
+        console.log(`            5 - Apagar um Jogo                       `);
+        console.log(`            0 - Encerrar                             `);
+        console.log(`                                                     `);
+        console.log(`*****************************************************`);
+        console.log(`                                                     `);
+
+        console.log(`Entre com a opcao desejada: `);
+        opcao = readLine.questionInt(`> : `);
+
+        if (opcao == 0) {
+            console.log(`Retro Games - Jogos antigos, jogadores eternos.`);
+            sobre();
+            process.exit(0);
+        }
+
+        switch(opcao) {
+            case 1: 
+                console.log(`\n\nAdicionar Novo Jogo\n\n`);
+                break;
+            case 2:
+                console.log(`\n\nExibir Coleção de Jogos\n\n`);
+                break;
+            case 3:
+                console.log(`\n\nProcurar um Jogo\n\n`);
+                break;
+            case 4:
+                console.log(`\n\nEditar um Jogo\n\n`);
+                break;
+            case 5:
+                console.log(`\n\nApagar um Jogo\n\n`);
+                break;
+            default:
+                console.log(`\n\nOpção inválida!\n\n`);
+                break;
+        }
+
+    }
+}
+
+
+export function sobre(): void {
+    console.log(`*****************************************************`);
+    console.log(`Projeto Desenvolvido por: Matheus Pereira Xavier`);
+    console.log(`Email: matheus97p.q@gmail.com`);
+    console.log(`GitHub: https://github.com/Matheus97px`);
+    console.log(`*****************************************************`);
+}
